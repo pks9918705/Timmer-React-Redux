801f6c154d9c0ea39d8ffadc9ea72a6057b1fe8d
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-07-12 18:21:58 +0530

    Explain Updated

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -20,3 +20,12 @@ export const Timer = () => {
 
   return <b>{elapsedTime}</b>;
 };
+
+
+//! Here Explain the working of Above Code 
+
+/*
+The return statement within the useEffect function is a cleanup function. It will be invoked when the component is unmounted or when the dependencies specified in the array change. In this case, it clears the interval using clearInterval to stop the timer and prevent any memory leaks.
+
+The dependencies array [isRunning, dispatch] specifies that the useEffect function should run whenever either isRunning or dispatch changes. If one of these values changes, the cleanup function is invoked, and the effect is re-run with the updated values.
+*/
